93f3b0e329ebe9e376390739f55a953e0c804b33
kongfuboy123 <kongfuboyjw2@gmail.com>
2021-07-30 14:57:27 +1000

    解构

--- ex1.js
+++ ex1.js
@@ -1,1 +1,11 @@
-console.log('hello')
+//解构对象
+let obj = {
+  name : 'tt',
+  age: 11
+}
+let {name,age} = obj;
+console.log(name,age);
+// 解构数组
+let arr1 = [1,2,3];
+let [a,b,c] = arr1;
+console.log(a,b,c);
