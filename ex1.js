0269fd2deeed62f26023b0cacc5b39709405e321
kongfuboy123 <kongfuboyjw2@gmail.com>
2021-07-30 16:46:05 +1000

    模板

--- ex1.js
+++ ex1.js
@@ -5,11 +5,17 @@ let obj = {
 }
 let {name,age} = obj;
 console.log(name,age);
+
 // 解构数组
 let arr1 = [1,2,3];
 let [a,b,c] = arr1;
 console.log(a,b,c);
-//部分解构
+//部分解构，数组需要位置对应，不如直接使用索引
 
 let [,e,f] = arr1;
 console.log(e,f); 
+console.log(arr1[1],arr1[2])
+//字符串解构
+let str1 = 'hello';
+let [g,h,l] = str1;
+console.log(g,h,l);
---
+++ ex2.js
@@ -0,0 +1,7 @@
+//模板字符串,不用再拼接了
+
+let obj = {
+  name : 'tt',
+  age: 11
+}
+console.log(`${obj.name}的年龄是${obj.age}`);
